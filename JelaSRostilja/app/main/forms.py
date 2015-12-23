880bd1c4dbe95a0255038fffd25214f812836dbb
Borna Ivanković <borna.ivankovic@fer.hr>
2015-12-23 17:26:41 +0100

    Dopunjena baza, implementirani komentari

---
+++ JelaSRostilja/app/main/forms.py
@@ -0,0 +1,15 @@
+﻿from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms.validators import DataRequired
+
+class UnosKorisnika(Form):
+    korisIme=StringField('Korisnicko ime', validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired()])
+    ime=StringField('Ime',validators=[DataRequired()])
+    prezime=StringField('Prezime',validators=[DataRequired()])
+    uloga=SelectField('Uloga',choices=[('Korisnik','Korisnik'),('Djelatnik','Djelatnik'),('Administrator','Administrator')])
+    submit=SubmitField('Dodaj')
+
+class UnosKomentara(Form):
+    tekst=StringField('Unesite komentar',validators=[DataRequired()])
+    submit=SubmitField('Komentiraj')
